7e2715f53ec54ea62f1fd0ad3933c02a3e1965ed
TheMadZergling <fefe0723456@gmail.com>
2016-10-26 11:24:29 +0200

    Regiter genyó

---
+++ Új Registrációs Dokumentum.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <title>Uj Szöveges Dokumentum</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="Új szöveges dokumentum.css"/>
+  </head>
+  <body>
+	<div class="container">
+  <form action="" method="post">
+    <div class="main">
+  <input type="text" name="usname" value="Felhasználónév"/>
+  <input type="password" name="password" value="Jelszó"/>
+  <input type="password" name="passwordconf" value="Jelszó Megerõsítése"/>
+  <input type="text" name="email" value="E-mail cím"/>
+  <input type="text" name="emailconf" value="E-mail cím Megerõsítése"/>
+  <input type="submit" name="button" value="Regisztáció"/>
+    </div>
+  </form>
+	</div>
+</body>
+</html>
