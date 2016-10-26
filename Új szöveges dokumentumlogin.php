e73f1afaa24b89d97780ffd6a046ff03f5a5fb1a
lilpp <lilpp9711@gmail.com>
2016-10-26 11:11:57 +0200

    Login
    Kész login tes sztem jó

---
+++ Új szöveges dokumentumlogin.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Új szöveges dokumentum</title>
+<!-- Include CSS File Here -->
+<link rel="stylesheet" href="Új szöveges dokumentum.css"/>
+<!-- Include JS File Here -->
+<script src="Új JÁVASKRIPT mappa/Új szöveges dokumentum.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="main">
+<h2>Lépj be az Uj szöveges dokumentum corporation weboldalára</h2>
+<form id="form_id" method="post" name="myform">
+<label>User Name :</label>
+<input type="text" name="username" id="username"/>
+<label>Password :</label>
+<input type="password" name="password" id="password"/>
+<input type="button" value="Login" id="submit" onclick="validate()"/>
+</form>
+</div>
+</div>
+</body>
+</html>
