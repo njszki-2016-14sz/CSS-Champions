66a103f6d0a69da3c4eaca43ac91f915ce67e91e
TheMadZergling <fefe0723456@gmail.com>
2016-10-27 14:04:42 +0200

    CSS és Kódolás
    A css buttonját átírtam és a kódolást átálítottam jah meg a todolist
    doksi

--- ujRegistraciosDokumentum.php
+++ ujRegistraciosDokumentum.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ï»¿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
-    <title>Uj Szöveges Dokumentum</title>
+    <title>Uj SzÃ¶veges Dokumentum</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="icon" type="image/png" href="Text-txt.svg"/>
 	<link rel="stylesheet" type="text/css" href="ujszovegesdokumentum.css"/>
@@ -13,13 +13,13 @@
 	<div class="container">
   <form action="" method="post">
     <div class="main">
-  <input type="text" name="usname" value="Felhasználónév"/>
+  <input type="text" name="usname" value="FelhasznÃ¡lÃ³nÃ©v"/>
   <span id="strength"></span>
   <input name="passwordi" id="password" type="password" onkeyup="passwordChanged();" />
-  <input type="password" name="passwordconf" value="Jelszó Megerõsítése"/>
-  <input type="text" name="email" value="E-mail cím"/>
-  <input type="text" name="emailconf" value="E-mail cím Megerõsítése"/>
-  <input type="submit" name="button" value="Regisztáció"/>
+  <input type="password" name="passwordconf" value="JelszÃ³ MegerÅ‘sÃ­tÃ©se"/>
+  <input type="text" name="email" value="E-mail cÃ­m"/>
+  <input type="text" name="emailconf" value="E-mail cÃ­m MegerÅ‘sÃ­tÃ©se"/>
+  <input type="submit" name="submit" value="RegisztÃ¡ciÃ³"/>
 
 	<?php
 	function registerf(){
@@ -39,15 +39,15 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO ujtabladokumentum (username, password, email)
 VALUES ('$name', '$password', '$email');";
 if ($conn->query($sql) === TRUE) {
-    echo "Sikeres regisztráció";
+    echo "Sikeres regisztrÃ¡ciÃ³";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 }
 print $password;
 print $passwordconf;
-if($password!=$passwordconf){echo "<h2>Nem egyeznek a jelszók</h2>";}
-if($email!=$emailconf){echo "<h2>Nem egyeznek az e-mailcímek</h2>";}
+if($password!=$passwordconf){echo "<h2>Nem egyeznek a jelszÃ³k</h2>";}
+if($email!=$emailconf){echo "<h2>Nem egyeznek az e-mailcÃ­mek</h2>";}
  }
  if(isset($_POST['send']))
 {
