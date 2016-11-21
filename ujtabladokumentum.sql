28debd2a0be67bf0a90eda2d5f099531e9ec5ca1
lilpp <lilpp9711@gmail.com>
2016-11-21 09:40:52 +0100

    uj adatbazis

--- ujtabladokumentum.sql
+++ ujtabladokumentum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2016. Okt 26. 12:33
+-- Létrehozás ideje: 2016. Nov 21. 09:39
 -- Kiszolgáló verziója: 5.7.11
 -- PHP verzió: 5.6.19
 
@@ -30,15 +30,23 @@ CREATE TABLE `ujtabladokumentum` (
   `id` int(11) NOT NULL,
   `username` varchar(26) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `email` varchar(26) NOT NULL
+  `email` varchar(26) NOT NULL,
+  `TeljesNev` varchar(28) DEFAULT NULL,
+  `Munka` varchar(20) DEFAULT NULL,
+  `Suli` varchar(35) DEFAULT NULL,
+  `Kapcsolat` varchar(12) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- A tábla adatainak kiíratása `ujtabladokumentum`
 --
 
-INSERT INTO `ujtabladokumentum` (`id`, `username`, `password`, `email`) VALUES
-(1, 'Bob', 'asd', 'bob@ujemail.docx');
+INSERT INTO `ujtabladokumentum` (`id`, `username`, `password`, `email`, `TeljesNev`, `Munka`, `Suli`, `Kapcsolat`) VALUES
+(1, 'Bob', 'asd', 'bob@ujemail.docx', 'Bob Bobinson', 'Bober', 'Elementary bob school', 'Complicated'),
+(2, 'fel', 'asd', 'asd', '', NULL, NULL, NULL),
+(3, 'ASD', 'asd', 'asd', '', NULL, NULL, NULL),
+(4, 'pici', 'asd', 'asd@asd.hu', NULL, NULL, NULL, NULL),
+(5, 'lolo', 'lo', 'asd@asd.com', NULL, NULL, NULL, NULL);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -58,7 +66,7 @@ ALTER TABLE `ujtabladokumentum`
 -- AUTO_INCREMENT a táblához `ujtabladokumentum`
 --
 ALTER TABLE `ujtabladokumentum`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
