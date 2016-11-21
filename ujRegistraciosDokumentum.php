43d738ec51b5caa81173f86ee6bbdec8208838d1
TheMadZergling <fefe0723456@gmail.com>
2016-11-21 12:17:24 +0100

    A felh név foglalt+ tologatás
    lásd a címben

--- ujRegistraciosDokumentum.php
+++ ujRegistraciosDokumentum.php
@@ -14,43 +14,55 @@
   <form action="" method="post">
     <div class="main">
 	<h2>Regisztráció</h2>
-  <span>Username</span>
-  <input type="text" name="usname" value=""/>
-  <span id="strength">Type Password</span>
-  <input name="password" id="password" type="password" onkeyup="passwordChanged();" onkeydown="passwordconfChanged()" />
-  <span id="pasconfspan">Confirm password</span> 
-  <input type="password" id="passwordconf" name="passwordconf" value="" onkeyup="passwordconfChanged();"/>
-  <span id="e-mailspan">Type your e-mail adress</span>
-  <input type="text" name="email" id="e-mail" value="" onkeyup="emailChanged()"/>
-  <span id="e-confspan">Confirm your e-mail adress</span>
-  <input type="text" name="emailconf" id="e-conf" value=""/>
-  <input type="submit" name="submit" value="Regisztáció"/>
-
-	<?php
+	<span>Username</span>
+	<input type="text" name="usname" value=""/>
+	<span id="strength">Type Password</span>
+	<input name="password" id="password" type="password" onkeyup="passwordChanged();" onkeydown="passwordconfChanged()" />
+	<span id="pasconfspan">Confirm password</span> 
+	<input type="password" id="passwordconf" name="passwordconf" value="" onkeyup="passwordconfChanged();"/>
+	<span id="e-mailspan">Type your e-mail adress</span>
+	<input type="text" name="email" id="e-mail" value="" onkeyup="emailChanged()"/>
+	<span id="e-confspan">Confirm your e-mail adress</span>
+	<input type="text" name="emailconf" id="e-conf" value=""/>
+	<input type="submit" name="submit" value="Regisztáció"/>
+	
+<?php
 	function registerf(){
-  $name=$_POST['usname'];
-  $password=$_POST['password'];
-  $passwordconf=$_POST['passwordconf'];
-  $email=$_POST['email'];
-  $emailconf=$_POST['emailconf'];
-  if (isset($name)&&isset($password)&&isset($passwordconf)&&isset($email)&&isset($emailconf)&&$password==$passwordconf&&$emailconf==$email) {
-$servername = "localhost";
-$username = "root";
-$dbname = "ujadatbazisdokumentum";
-$conn = new mysqli($servername, $username, "", $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "INSERT INTO ujtabladokumentum (username, password, email)
+	$name=$_POST['usname'];
+	$password=$_POST['password'];
+	$passwordconf=$_POST['passwordconf'];
+	$email=$_POST['email'];
+	$emailconf=$_POST['emailconf'];
+	if (isset($name)&&isset($password)&&isset($passwordconf)&&isset($email)&&isset($emailconf)&&$password==$passwordconf&&$emailconf==$email) 
+	{
+		$servername = "localhost";
+		$username = "root";
+		$dbname = "ujadatbazisdokumentum";
+		$conn = new mysqli($servername, $username, "", $dbname);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$sql = "SELECT * FROM ujtabladokumentum WHERE username LIKE ('$name');";
+		$asd=$conn->query($sql);
+		$result = $asd->fetch_row();
+		if ($result['1']!=$_POST['usname'])
+		{ 
+			$sql = "SELECT * FROM ujtabladokumentum WHERE email LIKE ('$email');";
+			$asd=$conn->query($sql);
+			$result = $asd->fetch_row();
+		if ($result['3']!=$_POST['email']) 
+		{	
+			$conn = new mysqli($servername, $username, "", $dbname);
+			if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "INSERT INTO ujtabladokumentum (username, password, email)
 VALUES ('$name', '$password', '$email');";
-if ($conn->query($sql) === TRUE) {
     echo "Sikeres regisztráció";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+	$conn->query($sql);
+	}else {echo "Error: " . "The email is already taken" . "<br>" . $conn->error;}
+}else {echo "Error: " . "The username is already taken" . "<br>" . $conn->error;}
 }
-}
-print $password;
-print $passwordconf;
 if($password!=$passwordconf){echo "<h2>Nem egyeznek a jelszók</h2>";}
 if($email!=$emailconf){echo "<h2>Nem egyeznek az e-mailcímek</h2>";}
  }
